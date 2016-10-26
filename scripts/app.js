81b811dd570c255f969a070e773b8e566c576485
Ben Manning <ben@hostfully.com>
2016-10-26 11:09:04 -0700

    fixed the update and display status

--- scripts/app.js
+++ scripts/app.js
@@ -11,17 +11,14 @@ $(document).ready(function(){
 	addSubHeader();
 
 
-	// the form redirects when it should just update the count of "E"s
+
 	$('form').on('submit', function(e){
 		e.preventDefault();
 		console.log('form submitted');
 		var input = $('input').eq(0).val();
 
-		// should count occurrences of the letter "E"
-		// and update the display. instead always shows 0
-		for (var i = 0; i < 10; i++){
-			// this loop doesn't find both es in  "supercalifragilisticexpialidocious"
-			var count = 0;
+		var count = 0;
+		for (var i = 0; i < input.length; i++){
 			if (input[i] === "e"){
 				count = count +1;
 			}
